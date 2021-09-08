6cecba2ceead7fecda5617d49c4d2719f9e69a5a
chouinardDavid <chouinard.dc@gmail.com>
2021-09-07 23:28:41 -0400

    modification to footer media queries

--- components/utils/sectionForm/sectionsForm.js
+++ components/utils/sectionForm/sectionsForm.js
@@ -1,12 +1,11 @@
-import TitleLabel from '../titleLabel/titleLabel';
 import styles from './sectionForm.module.scss';
 
 const SectionForm = (props) => {
     const { title, children, ...rest } = props;
     return (
         <div className={styles.sectionForm} {...rest}>
-            <div className={styles.title}>
-                <TitleLabel titleName={title} />
+            <div className={styles.titleContainer}>
+                <h1 className={styles.title}>{title}</h1>
             </div>
 
             <div className={styles.content}>{children}</div>
